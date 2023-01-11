074c7f4dd2aa2a9d784ec552acf802fabd3e2952
Max McDonough <max.mcdonough@gmail.com>
2023-01-11 14:21:56 -0500

    Added context to years match

--- experienceRegex.py
+++ experienceRegex.py
@@ -3,15 +3,25 @@
 import re, pyperclip
 
 # Regex for years of experience on job posting
-# Search for 'years', with number preceding
+# Search for 'year(s)', with number preceding
 yearsExperienceRegex = re.compile(r'''
     \d(?:-\d)?      # number of years (account for range)
     \+?             # with a plus (optional)
     \syear(?:s)?    # years with space before
 ''', re.VERBOSE)
 
+# Search for 'year(s), with context characters before and after
+yearsContextRegex = re.compile(r'''
+    .{0,20}         # Get 10 characters before match
+    \syear(?:s)?    # years with space before
+    .{0,100}         # Get 30 characters after match
+''', re.VERBOSE)
+
 # Get text off of clipboard
 textTest = pyperclip.paste()
 
-results = yearsExperienceRegex.findall(textTest)
-print(results)
+
+yearsExperience = yearsExperienceRegex.findall(textTest)
+yearsContext = yearsContextRegex.findall(textTest)
+print(f'Years experience: {yearsExperience}')
+print(f'Years context: {yearsContext}')
