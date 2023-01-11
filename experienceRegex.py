ae16c4376365401dcf7c98a33e08e61ab81765c3
Max McDonough <max.mcdonough@gmail.com>
2023-01-11 14:52:47 -0500

    skills regex

--- experienceRegex.py
+++ experienceRegex.py
@@ -12,16 +12,50 @@ yearsExperienceRegex = re.compile(r'''
 
 # Search for 'year(s), with context characters before and after
 yearsContextRegex = re.compile(r'''
-    .{0,20}         # Get 10 characters before match
+    .{0,20}         # Get 20 characters before match
     \syear(?:s)?    # years with space before
-    .{0,100}         # Get 30 characters after match
+    .{0,100}         # Get 100 characters after match
 ''', re.VERBOSE)
+
+# Search for list of skills
+skillsRegex = re.compile(r'''
+    (   # List skills found in job posting
+        front(?:\s|-)?end               |
+        full(?:\s|-)?stack              |
+        java(?:\s|-)?script(?:es6+)?    |
+        html                            |
+        css                             |
+        react(?:\s|-)?(?:native)?       |
+        redux                           |
+        node                            |
+        express                         |
+        python                          |
+        (?:no|my)?sql                   |
+        postgres                        |
+        mongo                           |
+        apollo                          |
+        graphql                         |
+        git                             |
+        cypress                         |
+        bootstrap                       |
+        j(?:\s|-)?query                 |
+        adobe                           |
+        amazon                          |
+        aws                             |
+        java                            |
+        python                          |
+        ios                             |
+        android                         |
+        swift)
+''', re.VERBOSE | re.IGNORECASE)
 
 # Get text off of clipboard
 textTest = pyperclip.paste()
 
-
 yearsExperience = yearsExperienceRegex.findall(textTest)
 yearsContext = yearsContextRegex.findall(textTest)
+skillsList = skillsRegex.findall(textTest)
+
 print(f'Years experience: {yearsExperience}')
 print(f'Years context: {yearsContext}')
+print(f'Skills listed: {skillsList}')
