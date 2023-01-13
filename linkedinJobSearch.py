27b7a83f93ec99c51d3db3d3f8b07db5ba561e98
Max McDonough <max.mcdonough@gmail.com>
2023-01-12 22:52:19 -0500

    Pull only necessary data, write to json file

--- linkedinJobSearch.py
+++ linkedinJobSearch.py
@@ -16,34 +16,28 @@ api = Linkedin(user, pw)
 # Search jobs
 jobResults = api.search_jobs(
     keywords='software developer',
-    experience=['2'],
     location_name='Atlanta, Georgia, United States',
     limit=10
 )
 
+pprint.pp(jobResults)
 # Iterate search results to get full job data by ID
 fullJobResults = []
 
 for i in range(len(jobResults) - 1):
     jobId = jobResults[i].get('dashEntityUrn').removeprefix('urn:li:fsd_jobPosting:') # get the value holding job number (and remove extraneous prefix)
-    # pprint.pp(jobId)
     job = api.get_job(jobId) # search for job by ID
-    # pprint.pp(job)
-    fullJobResults.append(job) # add to list
+
+    # Create new dictionary with only necessary data
+    basicJob = {}
+    basicJob['jobId'] = jobId
+    basicJob['title'] = job['title']
+    basicJob['company'] = job['companyDetails']['com.linkedin.voyager.deco.jobs.web.shared.WebCompactJobPostingCompany']['companyResolutionResult']['name']
+    basicJob['description'] = job['description']['text']
+    basicJob['listedAt'] = job['listedAt']
+
+    fullJobResults.append(basicJob) # add to list
 
 # pprint.pp(fullJobResults)
 jobFile = open('./jobTestFile.json', 'w')
 jobFile.write(json.dumps(fullJobResults))
-
-# # GET a profile
-# profile = api.get_profile('billy-g')
-
-# # GET a profiles contact info
-# contact_info = api.get_profile_contact_info('billy-g')
-
-# # GET 1st degree connections of a given profile
-# connections = api.get_profile_connections('1234asc12304')
-
-
-
-
