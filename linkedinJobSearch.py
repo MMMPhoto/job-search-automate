6cb6307c2b32a4876833b23c2c5abf9598780d4e
Max McDonough <max.mcdonough@gmail.com>
2023-01-24 12:47:05 -0500

    Test google sheets setup

--- linkedinJobSearch.py
+++ linkedinJobSearch.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 
 # Import modules
-import os, pprint, json
+import os, pprint, json, pygsheets
+import pandas as pd
 from linkedin_api import Linkedin
 from dotenv import load_dotenv
 from experienceRegex import yearsSearch, yearsContextSearch, skillsSearch
@@ -10,38 +11,56 @@ from experienceRegex import yearsSearch, yearsContextSearch, skillsSearch
 load_dotenv()
 user = os.getenv('LINKEDIN_USERNAME')
 pw = os.getenv('LINKEDIN_PW')
+gc = pygsheets.authorize(service_file='./job-search-service.json')
 
-# Autheniticate Linkedin account
-api = Linkedin(user, pw)
+# Create Dataframe
+df = pd.DataFrame()
 
-# Search jobs
-jobResults = api.search_jobs(
-    keywords='software developer',
-    location_name='Atlanta, Georgia, United States',
-    limit=10
-)
+# Create a column
+df['name'] = ['John', 'Steve', 'Sarah']
 
-pprint.pp(jobResults)
-# Iterate search results to get full job data by ID
-fullJobResults = []
+#open the google spreadsheet (string is name of sheet)
+sh = gc.open('Automated Job Search')
 
-for i in range(len(jobResults) - 1):
-    jobId = jobResults[i].get('dashEntityUrn').removeprefix('urn:li:fsd_jobPosting:') # get the value holding job number (and remove extraneous prefix)
-    job = api.get_job(jobId) # search for job by ID
+#select the first sheet 
+wks = sh[0]
 
-    # Create new dictionary with only necessary data
-    basicJob = {}
-    basicJob['jobId'] = jobId
-    basicJob['title'] = job['title']
-    basicJob['company'] = job['companyDetails']['com.linkedin.voyager.deco.jobs.web.shared.WebCompactJobPostingCompany']['companyResolutionResult']['name']
-    basicJob['description'] = job['description']['text']
-    basicJob['years experience'] = yearsSearch(job['description']['text'])
-    basicJob['years context'] = yearsContextSearch(job['description']['text'])
-    basicJob['skills'] = skillsSearch(job['description']['text'])
-    basicJob['listedAt'] = job['listedAt']
+#update the first sheet with df, starting at cell B2. 
+wks.set_dataframe(df,(1,1))
 
-    fullJobResults.append(basicJob) # add to list
 
-# pprint.pp(fullJobResults)
-jobFile = open('./jobTestFile.json', 'w')
-jobFile.write(json.dumps(fullJobResults))
+
+# # Autheniticate Linkedin account
+# api = Linkedin(user, pw)
+
+# # Search jobs
+# jobResults = api.search_jobs(
+#     keywords='software developer',
+#     location_name='Atlanta, Georgia, United States',
+#     limit=10
+# )
+
+# pprint.pp(jobResults)
+# # Iterate search results to get full job data by ID
+# fullJobResults = []
+
+# for i in range(len(jobResults) - 1):
+#     jobId = jobResults[i].get('dashEntityUrn').removeprefix('urn:li:fsd_jobPosting:') # get the value holding job number (and remove extraneous prefix)
+#     job = api.get_job(jobId) # search for job by ID
+
+#     # Create new dictionary with only necessary data
+#     basicJob = {}
+#     basicJob['jobId'] = jobId
+#     basicJob['title'] = job['title']
+#     basicJob['company'] = job['companyDetails']['com.linkedin.voyager.deco.jobs.web.shared.WebCompactJobPostingCompany']['companyResolutionResult']['name']
+#     basicJob['description'] = job['description']['text']
+#     basicJob['years experience'] = yearsSearch(job['description']['text'])
+#     basicJob['years context'] = yearsContextSearch(job['description']['text'])
+#     basicJob['skills'] = skillsSearch(job['description']['text'])
+#     basicJob['listedAt'] = job['listedAt']
+
+#     fullJobResults.append(basicJob) # add to list
+
+# # pprint.pp(fullJobResults)
+# jobFile = open('./jobTestFile.json', 'w')
+# jobFile.write(json.dumps(fullJobResults))
