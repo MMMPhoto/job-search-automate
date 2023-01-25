cfc4f748454fcb88a23ce9bb4e22017a645e4261
Max McDonough <max.mcdonough@gmail.com>
2023-01-25 11:06:53 -0500

    Check empty json file before opening, refactor create job dictionary

--- linkedinJobSearch.py
+++ linkedinJobSearch.py
@@ -8,16 +8,22 @@ from linkedin_api import Linkedin
 from dotenv import load_dotenv
 from experienceRegex import yearsSearch, yearsContextSearch, skillsSearch
 
-# Handle environment variables
+# Handle environment variables, authentication
 load_dotenv()
 user = os.getenv('LINKEDIN_USERNAME')
 pw = os.getenv('LINKEDIN_PW')
 googleSheets = pygsheets.authorize(service_file='./job-search-service.json')
 
-# Test data to dataframe
-with open('./jobTestFile.json', 'r') as file:
-  jobs = json.load(file)
-# pprint.pp(jobs)
+# Autheniticate Linkedin account
+api = Linkedin(user, pw)
+
+# Open local JSON file
+fileSize = os.stat('./jobSearchData.json').st_size
+print(fileSize)
+if fileSize != 0: 
+  with open('./jobSearchData.json', 'r') as file:
+    jobsInJson = json.load(file)
+  # pprint.pp(jobsInJson)
 
 #Open google spreadsheet, get existing job IDs
 sheet = googleSheets.open('Automated Job Search')
@@ -26,78 +32,60 @@ existingIds = wks.get_col(1, include_tailing_empty=False) # Get job numbers in c
 numRowsExisting = len(existingIds)
 del existingIds[0]
 
-# Clean up data
-for job in jobs:
+# Search new jobs
+searchResults = api.search_jobs(
+  keywords='software developer',
+  location_name='Atlanta, Georgia, United States',
+  limit=10
+)
+# pprint.pp(searchResults)
 
-  # Remove job if already in spreadsheet
-  if job['jobId'] in existingIds:
-    jobs.remove(job)
-  # pprint.pp(jobs)
+# Iterate search results to get full job data by ID
+newJobs = []
+for result in searchResults:
+  jobId = result.get('dashEntityUrn').removeprefix('urn:li:fsd_jobPosting:') # get the value holding job number (and remove extraneous prefix)
+  
+  # Check to make sure job is not in existing spreadsheet
+  if jobId not in existingIds:
 
-  # Add URL
-  id = job['jobId']
-  job['url'] = f'https://www.linkedin.com/jobs/view/{id}/'
+    job = api.get_job(jobId) # search for job by ID
+      
+    # Create new dictionary with only necessary data
+    jobClean = {}
+    jobClean['Date Added'] = dt.datetime.now().date()
+    jobClean['Job Id'] = jobId
+    jobClean['Url'] = f'https://www.linkedin.com/jobs/view/{jobId}/'
+    jobClean['Job Title'] = job['title']
+    jobClean['Company'] = job['companyDetails']['com.linkedin.voyager.deco.jobs.web.shared.WebCompactJobPostingCompany']['companyResolutionResult']['name']
+    jobClean['Description'] = job['description']['text']
+    jobClean['Years Experience'] = yearsSearch(job['description']['text'])
+    jobClean['Years Context'] = yearsContextSearch(job['description']['text'])
+    jobClean['Skills'] = skillsSearch(job['description']['text'])
 
-  # Add current date
-  dateAdded = dt.datetime.now().date()
-  job['Date Added'] = dateAdded
+    newJobs.append(jobClean) # add to list
 
-  # Format years experience
-  job['years experience'] = '\n'.join(job['years experience'])
+    # Format years experience
+    jobClean['years experience'] = '\n'.join(jobClean['years experience'])
 
-  # Format years context
-  contextList = job['years context']
-  contextFormat = []
-  for context in contextList:
-    contextFormat.append(f'....{context}....')
-  job['years context'] = '\n'.join(contextFormat)
+    # Format years context
+    contextList = jobClean['years context']
+    contextFormat = []
+    for context in contextList:
+      contextFormat.append(f'....{context}....')
+    jobClean['years context'] = '\n'.join(contextFormat)
 
-  # Format skills
-  skills = job['skills']
-  skillsTemp = []
-  skillsCount = []
-  for skill in skills:
-    if skill not in skillsTemp:
-      skillsTemp.append(skill)
-      count = skills.count(skill)
-      skillsCount.append(f'{skill}({count})')
-  job['skills'] = '\n'.join(skillsCount)
+    # Format skills
+    skills = jobClean['skills']
+    skillsTemp = []
+    skillsCount = []
+    for skill in skills:
+      if skill not in skillsTemp:
+        skillsTemp.append(skill)
+        count = skills.count(skill)
+        skillsCount.append(f'{skill}({count})')
+    jobClean['skills'] = '\n'.join(skillsCount)
 
-
-# # Autheniticate Linkedin account
-# api = Linkedin(user, pw)
-
-# # Search new jobs
-# searchResults = api.search_jobs(
-#     keywords='software developer',
-#     location_name='Atlanta, Georgia, United States',
-#     limit=10
-# )
-
-# # Print to console (test variable)
-# # pprint.pp(searchResults)
-
-# # Iterate search results to get full job data by ID
-# fullJobResults = []
-# for i in range(len(searchResults) - 1):
-#     jobId = searchResults[i].get('dashEntityUrn').removeprefix('urn:li:fsd_jobPosting:') # get the value holding job number (and remove extraneous prefix)
-#     job = api.get_job(jobId) # search for job by ID
-
-#     # Create new dictionary with only necessary data
-#     basicJob = {}
-#     basicJob['Job Id'] = jobId
-#     basicJob['Job Title'] = job['title']
-#     basicJob['Company'] = job['companyDetails']['com.linkedin.voyager.deco.jobs.web.shared.WebCompactJobPostingCompany']['companyResolutionResult']['name']
-#     basicJob['Description'] = job['description']['text']
-#     basicJob['Years Experience'] = yearsSearch(job['description']['text'])
-#     basicJob['Years Context'] = yearsContextSearch(job['description']['text'])
-#     basicJob['Skills'] = skillsSearch(job['description']['text'])
-
-#     fullJobResults.append(basicJob) # add to list
-
-# Print to console (test variable)
-# pprint.pp(fullJobResults)
-
+# pprint.pp(newJobs)
 
 # # Write to JSON file
 # jobFile = open('./jobTestFile.json', 'w')
